ff9833e6e93c7db279e5efce5cdd85395f8c6dfd
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-30 19:38:41 -0700

    Node Callbacks and Error Handling

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const rect = require('./rectangle');
 
 function solveRect(l, w) {
-  console.log(`Solving for rectangle with dimensions ${l}, ${w}`);
+  console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-  if (l <= 0 || w <= 0) {
-      console.log(`Rectangle dimensions should be greater than zero.
-          The values passed in were: ${l}, ${w}`);
-  } else {
-      console.log(`Area of rectangle: ${rect.area(l, w)}`);
-      console.log(`Perimeter of rectangle', ${rect.perimeter(l, w)}`);
-  }
+  rect(l, w, (err, rectangle) => {
+      if (err) { //if there's an error, console log the error message defined in rectangle.js
+          console.log('ERROR:', err.message);
+      } else {
+          console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+          console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+      }
+  });
+  console.log('This statement is logged after the call to rect()'); //To see the order in which the code is executed
 }
+//Asynchronous non-blocking code execution. Waits for the timers to finish and run the callbacks. But while waiting; still able to run other code. 
+// Because of closures; even though the rect function has finished executing by the time the callbacks from set timer run; the callbacks still had access to the length and width values passed into rect function.
 
 solveRect(2, 4);
 solveRect(3, 5);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,17 @@
-exports.perimeter = (x, y) => 2 * (x + y);
+//Creating a node module to import into another file. This is imported into app.js using the require function
 
-exports.area = (x, y) => x * y;
+//This export returns this entire function for this module instead of 2 seperate methods, so we needed to use module.exports instead of the shorthad (exports)
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) { //must be positive numbers
+    //asynchronous functions should return an error object as the first argument (for nodeJS)
+          callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`)); //node error callback pattern
+  } else { //if numbers were valid positive numbers
+      setTimeout(() =>
+          callback(null, { //null means no error
+              perimeter: () => 2 * (x + y), //The second argument is an object that contains two methods: perimeter and area.
+              area: () => x * y //inner functions have access to scope of the outer functions. Since x and y are available in the outer function; we dont need to pass them into these methods (thanks closures)
+          }),
+          2000 //miliseconds to wait
+      );
+  }
+};
